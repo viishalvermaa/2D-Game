5a156c21fe414c6afd9317cad8291ba1ac6fd165
Vishal Verma <verma.vishal3010@gmail.com>
2021-02-23 12:05:33 +0530

    Added tiles and player with movements

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce =1;
+
+    private Rigidbody2D _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
